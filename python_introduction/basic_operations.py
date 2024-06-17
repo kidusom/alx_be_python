b7f440df9556f644dedbb178ebace5646d856987
kidusom <kidusk2080@gmail.com>
2024-06-17 09:39:30 -0700

    modified:   python_introduction/basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,18 @@
-print (5)
+number1 = 10
+number2 = 5
+
+# Calculate the sum
+Addition  = number1 + number2
+print(f"Sum of {number1} and {number2} is {Addition}.")
+
+# Calculate the difference (number1 - number2)
+difference_result = number1 - number2
+print(f"Difference of {number1} and {number2} is {difference_result}.")
+
+# Calculate the product
+product_result = number1 * number2
+print(f"Product of {number1} and {number2} is {product_result}.")
+
+#Addition 
+#Subtraction  
+#Multiplication
