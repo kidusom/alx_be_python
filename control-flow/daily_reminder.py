ede8f52d081f35d6d81b2421484136edee4dc9a7
kidusom <kidusk2080@gmail.com>
2024-06-26 07:35:48 -0700

    modified:   daily_reminder.py 	modified:   multiplication_table.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,12 +2,7 @@ task = input('Enter your task:')
 priority = input('Priority (high/medium/low):')
 time_bound = input('Is it time-bound? (yes/no):')
 
-#highyes = print(f'{task} is a high priority task that requires immediate attention today!')
-#highno = print(f'{task} is a high priority task with no time bound')
-#medyes = print(f'{task} is a medium priority task with time bound')
-#medno = print(f'{task} is a medium priority task with no time bound')
-#lowyes = print(f'{task} is a low priority task with time bound')
-#lowno = print(f'{task} is a low priority task. Consider completing it when you have free time.')
+ 
 
  
 match priority:
--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,4 @@
 number = int(input("Enter a number to see its multiplication table:"))
 
-for i in range (1,11):
-       
-  print(f" {number} * {i} = {i*number} ")
-  
+for i in range(1, 11):
+    print(f" {number} * {i} = {i*number} ")
