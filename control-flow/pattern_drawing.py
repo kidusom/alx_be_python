ecafac2b95e18a0eb45873fb791f7fd5286e1c2c
kidusom <kidusk2080@gmail.com>
2024-06-26 02:22:00 -0700

    modified:   weather_advice.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,14 @@
+size = int(input('Enter the size of the pattern:'))
+
+if size <= 0:
+    print('input a postive number')
+else :
+    row = 0
+
+while row < size:
+ for x in range(size):
+        print('*',end="")
+
+print()
+row =+ 1
+
