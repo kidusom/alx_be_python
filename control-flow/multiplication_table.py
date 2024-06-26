4c88aadaa6ee21e9d09d6cfd31f5638ce7cf79a2
kidusom <kidusk2080@gmail.com>
2024-06-26 03:26:11 -0700

    modified:   daily_reminder.py 	modified:   multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,5 @@ number = float(input("Enter a number to see its multiplication table:"))
 
 for i in range (1,11):
        
-  print(number,'*',i , '=' , i*number)
-    
-
+  print (f'{number} * {i} = {i*number}')
+  
