3e9a08bfb46f8092be28c7e94161a261d8369a6b
kidusom <kidusk2080@gmail.com>
2024-07-01 00:01:16 -0700

    modified:   arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,5 @@
 from arithmetic_operations import perform_operation
 
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
 def perform_operation(num1: float, num2: float, operation: str):
     match operation :
             
@@ -22,6 +13,17 @@ def perform_operation(num1: float, num2: float, operation: str):
             if num2 !=0: 
                return num1/num2
             else: print("second number cant be zero")   
+
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
 
 if __name__ == "__main__":
     main()
