38061ddacba406c72c1a0f7456d3dd1f9f393e85
kidusom <kidusk2080@gmail.com>
2024-06-30 23:35:40 -0700

    modified:   arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,8 +4,22 @@ def main():
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+    match operation :
+            
+     case 'add' :
+           ans = num1 + num2
+     case 'subtract' :
+            ans = num1 - num2
+
+     case 'multiply':
+            ans = num1 * num2
+     case 'divide' :    
+            if num2 !=0: 
+               ans =num1/num2
+            else: print("second number cant be zero")            
+
+     
+    print(f"Result: {ans}")
 
 if __name__ == "__main__":
     main()
